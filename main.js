d4a0ce835e02d8b9fd07bf60b4692bdce0f02a2c
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-18 22:42:39 +0530

    refactor: updated code and packages

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement(
   'div',
   { id: 'parent' },
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'I am Ironman'),
-    React.createElement('h1', {}, 'I am Spidey'),
+    React.createElement('h1', { key: 'iron' }, 'I am Ironman'),
+    React.createElement('h1', { key: 'spidey' }, 'I am Spidey'),
   ])
 );
 
